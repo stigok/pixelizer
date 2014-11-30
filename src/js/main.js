6d0e22ffe5ba8d93f726d310cd33a9333cd92f58
stigok <stig@stigok.com>
2014-11-30 07:19:12 +0100

    Second ed

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,56 @@
-var canvas, ctx;
+var canvas, ctx, zoom, pixlz;
 
 $(function() {
 
     canvas = document.getElementById("mainCanvas");
     ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
 
-    $.getJSON('js/testimage.json', handleImage);
+    //var $slider = $("#slider" ).slider({
+    //    value:.25,
+    //    min: .25,
+    //    max: 2,
+    //    step: .25,
+    //    slide: function(event, ui) {
+    //        zoom = ui.value;
+    //        drawData();
+    //    }
+    //});
+
+    $.ajax({
+        dataType: "json",
+        url: 'js/testimage.json',
+        success: handleImage,
+        async: false
+    });
 
 });
 
-function handleImage(pixlz) {
-    canvas.width = pixlz.w;
-    canvas.height = pixlz.h;
+
+
+function handleImage(data) {
+    pixlz = data;
+
+    console.time("DrawCanvas10");
+    for (var i = 1; i <= 10; i++); {
+        zoom = .25 * i;
+        drawData();
+    }
+    console.timeEnd("DrawCanvas10");
+
+}
+
+function drawData() {
+    canvas.width = pixlz.w * zoom;
+    canvas.height = pixlz.h * zoom;
 
     var p;
     for (var i = 0; i < pixlz.items.length; i++) {
         p = pixlz.items[i];
-        fillArea(p.Color, p.X, p.Y, p.X + pixlz.s, p.Y + pixlz.s);
-        console.log("paint: %ix%i : %s", p.X, p.Y, colorToRGBA(p.Color));
+        fillArea(p.Color, p.X * zoom, p.Y * zoom, (p.X + pixlz.s) * zoom, (p.Y + pixlz.s) * zoom);
     }
+
+    console.log("%i items painted", i);
 }
 
 function fillArea(color, x1, y1, x2, y2) {
@@ -27,6 +59,6 @@ function fillArea(color, x1, y1, x2, y2) {
 }
 
 function colorToRGBA(c) {
-    var a = (c.A / 255).toFixed(1);
+    var a = (c.A / 255);
     return "rgba(" + c.R + "," + c.G + "," + c.B + "," + a + ")";
 }
---
+++ src/js/test.js
@@ -0,0 +1,4 @@
+QUnit.test( "speed test", function( assert ) {
+    
+    assert.ok( 1 == "1", "Passed!" );
+});
